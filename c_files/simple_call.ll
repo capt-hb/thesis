d1f4e9d1ac3ee5c192d4572104149d226927b221
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2020-02-22 14:36:32 +0100

    small fix, assembly and llvm IR generated for CHERI256 instead of 128

--- c_files/simple_call.ll
+++ c_files/simple_call.ll
@@ -1,9 +1,8 @@
 ; ModuleID = 'simple_call.c'
 source_filename = "simple_call.c"
-target datalayout = "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128-A200-P200-G200"
-target triple = "cheri-unknown-freebsd-purecap"
-
-; Function Attrs: noinline nounwind optnone
+target datalayout = "E-m:e-pf200:256:256:256:64-i8:8:32-i16:16:32-i64:64-n32:64-S256-A200-P200-G200"
+target triple = "mips64c256-unknown-freebsd-purecap"
+; Function Attrs: noinline nounwind optnone uwtable
 define signext i32 @doSomething(i32 signext %a) addrspace(200) #0 {
 entry:
   %a.addr = alloca i32, align 4, addrspace(200)
@@ -11,8 +10,7 @@ entry:
   %0 = load i32, i32 addrspace(200)* %a.addr, align 4
   ret i32 %0
 }
-
-; Function Attrs: noinline nounwind optnone
+; Function Attrs: noinline nounwind optnone uwtable
 define signext i32 @main() addrspace(200) #0 {
 entry:
   %retval = alloca i32, align 4, addrspace(200)
@@ -24,11 +22,11 @@ entry:
   ret i32 %0
 }
 
-attributes #0 = { noinline nounwind optnone "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-frame-pointer-elim"="false" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="beri" "target-features"="+beri,+cheri128,+chericap,-noabicalls" "unsafe-fp-math"="false" "use-soft-float"="false" }
+attributes #0 = { noinline nounwind optnone uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "frame-pointer"="none" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="beri" "target-features"="+beri,+cheri256,+chericap,+soft-float,-noabicalls" "unsafe-fp-math"="false" "use-soft-float"="true" }
 
 !llvm.module.flags = !{!0, !1}
 !llvm.ident = !{!2}
 
 !0 = !{i32 1, !"wchar_size", i32 4}
 !1 = !{i32 7, !"PIC Level", i32 1}
-!2 = !{!"clang version 9.0.0 (git@github.com:capt-hb/llvm-project.git 39c89f7d086070b149443c4bd3ca31c30ba14c97)"}
+!2 = !{!"clang version 11.0.0 (git@github.com:capt-hb/llvm-project.git faf2ba46aca02fded4873b839960fea8f99094cc)"}
--- c_files/simple_call.s
+++ c_files/simple_call.s
@@ -2,8 +2,9 @@
 	.abicalls
 	.section	.mdebug.abi64,"",@progbits
 	.nan	legacy
-	.file	"simple_call.c"
+	.module	softfloat
 	.text
+	.file	"simple_call.c"
 	.globl	doSomething             # -- Begin function doSomething
 	.p2align	3
 	.type	doSomething,@function
@@ -11,20 +12,22 @@
 	.set	nomips16
 	.ent	doSomething
 doSomething:                            # @doSomething
-	.frame	$c11,16,$c17
+	.cfi_startproc
+	.frame	$c11,32,$c17
 	.mask 	0x00000000,0
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
 	.set	noat
 # %bb.0:                                # %entry
-	cincoffset	$c11, $c11, -16
+	cincoffset	$c11, $c11, -32
+	.cfi_def_cfa_offset 32
                                         # kill: def $a0 killed $a0 killed $a0_64
-	cincoffset	$c1, $c11, 12
+	cincoffset	$c1, $c11, 28
 	csetbounds	$c1, $c1, 4
 	csw	$4, $zero, 0($c1)
 	clw	$2, $zero, 0($c1)
-	cincoffset	$c11, $c11, 16
+	cincoffset	$c11, $c11, 32
 	cjr	$c17
 	nop
 	.set	at
@@ -33,6 +36,7 @@ doSomething:                            # @doSomething
 	.end	doSomething
 .Lfunc_end0:
 	.size	doSomething, .Lfunc_end0-doSomething
+	.cfi_endproc
                                         # -- End function
 	.globl	main                    # -- Begin function main
 	.p2align	3
@@ -41,37 +45,39 @@ doSomething:                            # @doSomething
 	.set	nomips16
 	.ent	main
 main:                                   # @main
-	.frame	$c11,64,$c17
+	.cfi_startproc
+	.frame	$c11,128,$c17
 	.mask 	0x00000000,0
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
 	.set	noat
 # %bb.0:                                # %entry
-	cincoffset	$c11, $c11, -64
-	csc	$c17, $zero, 48($c11)   # 16-byte Folded Spill
-	lui	$1, %hi(%neg(%captab_rel(main)))
-	daddiu	$1, $1, %lo(%neg(%captab_rel(main)))
-	cincoffset	$c26, $c12, $1
-	cmove	$c1, $c26
-	cincoffset	$c2, $c11, 44
+	cincoffset	$c11, $c11, -128
+	.cfi_def_cfa_offset 128
+	csc	$c17, $zero, 96($c11)   # 32-byte Folded Spill
+	.cfi_offset 89, -32
+	lui	$1, %pcrel_hi(_CHERI_CAPABILITY_TABLE_-8)
+	daddiu	$1, $1, %pcrel_lo(_CHERI_CAPABILITY_TABLE_-4)
+	cgetpccincoffset	$c1, $1
+	cincoffset	$c2, $c11, 92
 	csetbounds	$c2, $c2, 4
-	cincoffset	$c3, $c11, 40
+	cincoffset	$c3, $c11, 88
 	csetbounds	$c3, $c3, 4
 	addiu	$2, $zero, 0
 	csw	$zero, $zero, 0($c2)
 	clcbi	$c12, %capcall20(doSomething)($c1)
 	daddiu	$4, $zero, 100
 	cgetnull	$c13
-	csc	$c3, $zero, 16($c11)    # 16-byte Folded Spill
-	csw	$2, $zero, 12($c11)     # 4-byte Folded Spill
+	csc	$c3, $zero, 32($c11)    # 32-byte Folded Spill
+	csw	$2, $zero, 28($c11)     # 4-byte Folded Spill
 	cjalr	$c12, $c17
 	nop
-	clc	$c1, $zero, 16($c11)    # 16-byte Folded Reload
+	clc	$c1, $zero, 32($c11)    # 32-byte Folded Reload
 	csw	$2, $zero, 0($c1)
 	clw	$2, $zero, 0($c1)
-	clc	$c17, $zero, 48($c11)   # 16-byte Folded Reload
-	cincoffset	$c11, $c11, 64
+	clc	$c17, $zero, 96($c11)   # 32-byte Folded Reload
+	cincoffset	$c11, $c11, 128
 	cjr	$c17
 	nop
 	.set	at
@@ -80,9 +86,9 @@ main:                                   # @main
 	.end	main
 .Lfunc_end1:
 	.size	main, .Lfunc_end1-main
+	.cfi_endproc
                                         # -- End function
-
-	.ident	"clang version 9.0.0 (git@github.com:capt-hb/llvm-project.git 39c89f7d086070b149443c4bd3ca31c30ba14c97)"
+	.ident	"clang version 11.0.0 (git@github.com:capt-hb/llvm-project.git faf2ba46aca02fded4873b839960fea8f99094cc)"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
 	.addrsig_sym doSomething
